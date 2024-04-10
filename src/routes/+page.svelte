a324799e7053fef62e7f582a9ce52cb8c84558b4
Slaven Ivanov <slaven.ivanov.95@gmail.com>
2024-04-10 17:27:44 +0300

    Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { Button } from '$lib/components/ui/button'
+</script>
+
+<div class="d h-screen flex flex-col items-center justify-center">
+  <div>Hi 👽</div>
+  <Button>Press me</Button>
+</div>
